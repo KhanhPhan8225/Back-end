ef92696a742a89d06245b96fd7401545df4ca149
Trọng Tùng <108003841+TrTung216@users.noreply.github.com>
2023-08-20 16:22:40 +0700

    Update nenep.php

--- nenep.php
+++ nenep.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
-    <title>Bảng Thi Đua</title>
+    <title>Bảng Nề Nếp</title>
 </head>
 <body>
     <?php require_once "class.database.php"?>
