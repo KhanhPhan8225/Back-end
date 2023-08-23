8a72e9fc1e8edf05d0faee1774044733c425d051
Trọng Tùng <108003841+TrTung216@users.noreply.github.com>
2023-08-23 23:49:29 +0700

    Update nhanloi.php

--- nhanloi.php
+++ nhanloi.php
@@ -1,3 +1,8 @@
+<?php
+include "class.database.php";
+global $conn;
+$result=mysqli_query($conn,"SELECT * FROM baoloi");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,19 +11,36 @@
     <title>Trang báo nhập sai</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
 </head>
-<body>
-    <?php
+<?php
         session_start();
+        if(empty($_SESSION['id_lop'])){
+            header('location:login.php');
+        }
     ?>
-    <?php
-       echo $_SESSION['lop']
-    ?>
-    <div class="container">
-        <form method="POST" action="">
-            <div>
-                <a href="logout.php" class="btn btn-info" role="button">Logout</a>
-            </div>
-        </form>
-    </div>
+<body id="page-top">
+     <div class="card-body">
+		<div class="table-responsive">
+			<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+				<thead>
+					<tr>
+						<th>Lớp</th>
+						<th>Báo lỗi</th>
+                        <th>Tuần thi đua</th>   						
+					</tr>
+				</thead>
+			   
+				<tbody>
+				<?php while($row=mysqli_fetch_assoc($result)):?>
+					<tr>
+						<td><?=$row['ten_lop']?></td>
+						<td><?=$row['bao_loi']?></td>
+                        <td><?=$row['tuan']?></td>
+					</tr>
+					<?php endwhile; ?>
+				  
+				</tbody>
+			</table>
+		</div>
+	</div>
 </body>
 </html>
