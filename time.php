3e74b5c6d5cdcd33cbfc5fb9b0f8c2a5c868a9fd
Trọng Tùng <108003841+TrTung216@users.noreply.github.com>
2023-08-23 22:20:47 +0700

    Update time.php

--- time.php
+++ time.php
@@ -4,6 +4,7 @@
     #$list= timezone_identifiers_list();
     #print_r(getdate());
     $_SESSION['thu']=date('w');
+    #print(date('w'));
     $_SESSION['gio']=date('G');
     $_SESSION['phut']=date('i')
     #print_r($list);
@@ -13,9 +14,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
     <title>404-NOT FOUND</title>
 </head>
 <body>
     <h2>Chưa đến thời gian đăng nhập, vui lòng chờ.</h2>
+    <hr>
+    <div>
+        <a href="change-password.php" class="btn btn-info" role="button">Đổi mật khẩu</a>
+    </div>
+    <hr>
+    <div>
+        <a href="logout.php" class="btn btn-info" role="button">Logout</a>
+    </div>
 </body>
 </html>
